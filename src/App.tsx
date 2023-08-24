c2e5e42474aded6e45a557634646a0a978faf802
joban <91016674+acrcw@users.noreply.github.com>
2023-08-24 15:27:48 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import theme from './theme'; // Import your created theme
-import LandingPage from './Components/LandingPage';
-import { BrowserRouter as Router, Routes, Route,Navigate  } from 'react-router-dom'; // Import Router and Routes
-import SecondPage from './Components/SecondPage';
+import React from "react";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import theme from "./theme"; // Import your created theme
+import LandingPage from "./Components/LandingPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom"; // Import Router and Routes
+import SecondPage from "./Components/SecondPage";
 function App() {
   return (
     <ThemeProvider theme={theme}>
